6b934997bc4d32a157d2f32a18daf6ba104bcf3c
Luisa Farias de Macedo <luisafariasdemacedo@gmail.com>
2022-08-31 21:14:56 -0300

    feat: adicionado o bound confiner pelo mapa
    O bound confiner se adapta ao mapa, permitindo que o personagem consiga andar e o mapa, nas extremidades, não "vá para fora". Aula 5 - Add Cinemachine Confiner.

---
+++ Jogo RPG 2D/Assets/Scripts/Scene/SwitchConfineBoundingShape.cs
@@ -0,0 +1,31 @@
+﻿
+using Cinemachine;
+using UnityEngine;
+
+public class SwitchConfineBoundingShape : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SwitchBoundingShape();
+    }
+
+    /// <summary>
+    /// Switch the collider that cinemachine uses to define the edges of the screen
+    /// </summary>
+    private void SwitchBoundingShape()
+    {
+        //Get the polygon collider on the "boundconfiner" gameobjetc which is used by CineMachine to prevent the camera going beyond the screnn edges 
+        PolygonCollider2D polygonCollider2D = GameObject.FindGameObjectWithTag(Tags.BoundsConfiner).GetComponent<PolygonCollider2D>();
+
+
+        CinemachineConfiner cinemachineConfiner = GetComponent<CinemachineConfiner>();
+
+        cinemachineConfiner.m_BoundingShape2D = polygonCollider2D;
+
+        //clear the cache
+        cinemachineConfiner.InvalidatePathCache();
+    
+        
+    }
+}

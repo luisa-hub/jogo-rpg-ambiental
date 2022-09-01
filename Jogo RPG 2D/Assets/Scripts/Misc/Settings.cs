3363231321adca613fa86cd0ed7a449fd50cbb5a
Luisa Farias de Macedo <luisafariasdemacedo@gmail.com>
2022-08-31 22:37:56 -0300

    feat: adicionado o obscure fader nos itens do cenário
    Quando o personagem anda por trás dos itens do jogo que contém os scripts de Fade In e Fade Out, o objeto aos pouco se suaviza. Aula 6 - scenary fader

--- Jogo RPG 2D/Assets/Scripts/Misc/Settings.cs
+++ Jogo RPG 2D/Assets/Scripts/Misc/Settings.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 
 public static class Settings
 {
+    //Obscuring item fading
+    public const float fadeInSeconds = 0.25f;
+    public const float fadeOutSeconds = 0.35f;
+    public const float targetAlpha = 0.45f;
 
     //PLayer Movement
     public const float runningSpeed = 5.333f;

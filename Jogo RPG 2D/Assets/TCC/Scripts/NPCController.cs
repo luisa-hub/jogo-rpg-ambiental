6a452bf79c450c4247edc1d3a94975da452d723b
Luisa Farias de Macedo <luisafariasdemacedo@gmail.com>
2022-09-02 11:46:39 -0300

    feat: adicionado o protótipo de diálogo com NPCs
    Dia 02/09 com o professor Leonardo

---
+++ Jogo RPG 2D/Assets/TCC/Scripts/NPCController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour
+{
+
+    private GameObject chatCanvas;
+    public string text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        chatCanvas = GameObject.Find("Canvas").transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InitInteraction()
+    {
+        chatCanvas.GetComponentInChildren<ChatController>().SetVisible(true);
+        // Bloqueia o input do teclado para o personagem
+        GameObject.Find("Player").GetComponent<Player>().PlayerMovementState(false);
+    }
+
+
+}
---
+++ Jogo RPG 2D/Assets/TCC/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private GameObject lastGameObjectWithCollision;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Return))
+        {
+            if (lastGameObjectWithCollision && lastGameObjectWithCollision.tag.Equals("NPC"))
+            {
+                NPCController nPCController = lastGameObjectWithCollision.GetComponent<NPCController>();
+                nPCController.InitInteraction();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        lastGameObjectWithCollision = collision.gameObject;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        lastGameObjectWithCollision = null;
+    }
+}

be446bc5daea7ed198c02a518ce56cea44293f5e
Luisa Farias de Macedo <luisafariasdemacedo@gmail.com>
2022-10-14 20:30:15 -0300

    feat: adicionada lógica de verificação no QuestController
    Verifica se o usuário digitou o esperado

--- Jogo RPG 2D/Assets/TCC/Scripts/QuestsController.cs
+++ Jogo RPG 2D/Assets/TCC/Scripts/QuestsController.cs
@@ -10,69 +10,71 @@ namespace Assets.TCC.Scripts
         public DatabaseDB banco;
         private void Start()
         {
-            
+
         }
 
 
-        public void verifyData(List<string> coluna, List<IList<object>> linha) {
-            coluna.Sort();
-           
+        public void verifyData(List<string> coluna, List<IList<object>> linhas)
+        {
+
+
             //Proximos passos: vamos tentar transformar a List<IList<object>> numa lista de strings para 
             //poder mexer ocm ela com mais facilidade
-            bool teste = true;
-            bool teste2 = true;
-            List<string> colunaEsperada = banco.colunas("SELECT * FROM user;", "exemplo");
-            colunaEsperada.Sort();
-            List<IList<object>> linhaEsperada = banco.dados("SELECT * FROM user;", "exemplo");
+            bool resultadoColunas = true;
+            bool resultadoLinhas = true;
+          
+            
+            List<IList<object>> linhasEsperadas = banco.dados("SELECT * FROM user;", "exemplo");
 
-            var firstNotSecond = linha.Except(linhaEsperada).ToList();
-            var secondNotFirst = linhaEsperada.Except(linha).ToList();
-
-            var firstNotSecondc = coluna.Except(colunaEsperada).ToList();
-            var secondNotFirstc = colunaEsperada.Except(coluna).ToList();
+            List<string> colunasEsperadas = banco.colunas("SELECT * FROM user;", "exemplo");
+            var firstNotSecondc = coluna.Except(colunasEsperadas).ToList();
+            var secondNotFirstc = colunasEsperadas.Except(coluna).ToList();
 
 
-            Debug.Log($"Colunas:{!firstNotSecondc.Any() && !secondNotFirstc.Any()}");
+            resultadoColunas = !firstNotSecondc.Any() && !secondNotFirstc.Any();
 
-            foreach (var listinha in linhaEsperada)
+            if (!resultadoColunas)
             {
-                foreach (var lista in listinha)
+                Debug.Log("Acabou aqui, colunas erradas!");
+                return;
+            }
+
+            if (linhasEsperadas.Count != linhas.Count)
+            {
+                Debug.Log("Acabou aqui, linhas erradas!");
+                resultadoLinhas = false;
+            }
+
+            else
+            {
+                foreach (var linhaEsperada in linhasEsperadas)
                 {
-                    Debug.Log("testando");
-                    if (!linha.Contains(lista))
+                    foreach (var linha in linhas)
                     {
-                        teste = false;
-                        return;
+                        var firstNotSecond = linha.Except(linhaEsperada).ToList();
+                        var secondNotFirst = linhaEsperada.Except(linha).ToList();
+                        resultadoLinhas = !firstNotSecondc.Any() && !secondNotFirstc.Any();
+
+                        if (!resultadoLinhas)
+                        {
+                            Debug.Log("Acabou aqui, linhas erradas!");
+                            break;
+                        }
                     }
                 }
             }
 
-            Debug.Log($"Testando a linhaa: {teste}");
-               
-
-            
-
-            //foreach (var item in coluna)
-            //{
-            //    if (!colunaEsperada.Contains(item))
-            //    {
-            //        teste2 = false;
-            //        continue;
-            //    }
-
-            //}
-            //Debug.Log($"Colunas: {coluna.Equals(colunaEsperada)} ");
-            //Debug.Log($"Colunas: {teste2} ");
-
-            //Debug.Log($"Teste: {teste} ");
-            if (coluna==colunaEsperada && teste) {
+            if (resultadoColunas && resultadoLinhas)
+            {
                 Debug.Log("YEEEEY");
             }
 
         }
-        public void verifyTalk(string entrada) { 
-        
+        public void verifyTalk(string entrada)
+        {
+
         }
+
 
     }
 }

c5c2b36a1e2ea4a687e806a8c1528514c23cb9dd
Luisa Farias de Macedo <luisafariasdemacedo@gmail.com>
2022-11-06 23:23:22 -0300

    testando

---
+++ Jogo RPG 2D/Assets/TCC/Scripts/SQLBook/AppEvents.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public static class AppEvent
+{
+ 
+    public static event System.EventHandler OpenBook;
+
+    public static event System.EventHandler CloseBook;
+
+    public static void CloseBookFunction()
+    {
+        if (CloseBook != null)
+            CloseBook(new object(), new EventArgs());
+
+    }
+
+    public static void OpenBookFunction()
+    {
+        if (OpenBook != null)
+            OpenBook(new object(), new EventArgs());
+    }
+
+}
---
+++ Jogo RPG 2D/Assets/TCC/Scripts/SQLBook/OpenBook.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class OpenBook : MonoBehaviour
+{
+    [SerializeField] Button openBtn = null;
+    public Vector3 rotationVector;
+
+    private Quaternion startRotation; 
+
+    bool isOpenClicked;
+    private DateTime startTime;
+    private DateTime endTime;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startRotation = transform.rotation;
+
+        if (openBtn != null)
+            openBtn.onClick.AddListener(() => openBtn_Click());
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOpenClicked)
+            transform.Rotate(rotationVector * Time.deltaTime);
+        endTime = DateTime.Now;
+
+        if ((endTime - startTime).TotalSeconds >= 1)
+        {
+            isOpenClicked = false;
+            Vector3 newRotation = new Vector3(startRotation.x, 100, startRotation.y);
+            transform.rotation = Quaternion.Euler(newRotation);
+        
+        }
+
+        
+    }
+
+    public void openBtn_Click()
+    {
+        isOpenClicked = true;
+        startTime = DateTime.Now;
+
+        rotationVector = new Vector3(0, 100, 0);
+
+    }
+}
